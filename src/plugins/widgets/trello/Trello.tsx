45f84eeca0a3a7edc66d75b2d72ca32c15856782
John Ling <johnlingbusiness@gmail.com>
2025-11-04 08:54:16 +1100

    feat: changed to just use Trello authentication

--- src/plugins/widgets/trello/Trello.tsx
+++ src/plugins/widgets/trello/Trello.tsx
@@ -1,5 +1,6 @@
 import React, { FC, useEffect } from "react";
 import { defaultData, Props } from "./types";
+import "./Trello.sass";
 
 const Trello: FC<Props> = ({data = defaultData, setData}) => {
   return (
