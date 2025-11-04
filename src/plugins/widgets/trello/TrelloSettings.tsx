b502214262d37bbc8d245206a043e8e4cd2d545c
John Ling <johnlingbusiness@gmail.com>
2025-11-04 11:20:52 +1100

    feat: create popup for authentication
    - added identity to manifest permissions for browser

--- src/plugins/widgets/trello/TrelloSettings.tsx
+++ src/plugins/widgets/trello/TrelloSettings.tsx
@@ -1,20 +1,30 @@
-import React, { FC } from "react";
+import React, { FC, useEffect } from "react";
 import { defaultData, Props } from "./types";
 import Button from "../../../views/shared/Button";
 import { FormattedMessage } from "react-intl";
 
 const TrelloSettings: FC<Props> = ({ data = defaultData, setData}) => {
   const CALLBACK_URL = "https://trellocallback-rrswz5h5iq-de.a.run.app";
+  console.log(CALLBACK_URL);
   const AUTH_URL = "https://trello.com/1/authorize" +
       "?expiration=1day" +
       "&callback_method=fragment" + 
-      `&return_url=${encodeURIComponent(CALLBACK_URL)}` + 
       "&scope=read" + 
       "&response_type=token" + 
       `&key=${TRELLO_API_KEY}`
-
-  const onAuthenticateClick = () => {
-    window.location.href = AUTH_URL
+  
+  const onAuthenticateClick = async () => {
+    const redirectUrl = browser.identity.getRedirectURL();
+    console.log(redirectUrl);
+    const authUrl = `${AUTH_URL}&return_url=${encodeURIComponent(redirectUrl)}`;
+    console.log(authUrl);
+    const redirectResponse = await browser.identity.launchWebAuthFlow({
+      url: authUrl,
+      interactive: true
+    });
+    const tokenMatch = redirectResponse.match(/token=([^&]+)/);
+    const token = tokenMatch ? tokenMatch[1] : null;
+    console.log(token);
   }
 
   return (
