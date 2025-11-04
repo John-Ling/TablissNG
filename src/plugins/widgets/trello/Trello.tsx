d1d85d62aaaacc865d334796f0f181a6fddbfd56
John Ling <johnlingbusiness@gmail.com>
2025-11-04 11:20:52 +1100

    feat: create popup for authentication
    - added identity to manifest permissions for browser

--- src/plugins/widgets/trello/Trello.tsx
+++ src/plugins/widgets/trello/Trello.tsx
@@ -3,6 +3,24 @@ import { defaultData, Props } from "./types";
 import "./Trello.sass";
 
 const Trello: FC<Props> = ({data = defaultData, setData}) => {
+  const onEvent = (event: MessageEvent<any>) => {
+    if (event.origin === "https://trellocallback-rrswz5h5iq-de.a.run.app") {
+      console.log("Received event");
+      console.log(event.data);
+    }
+  }
+
+  useEffect(() => {
+    console.log("Attaching event listener");
+    window.addEventListener("message", onEvent);
+
+    return () => {
+      console.log("Removing event listener");
+      window.removeEventListener("message", onEvent);
+    }
+  }, []);
+
+
   return (
     <>
     </>
