3d8b97f331672ba991095b70002a205e345319ce
John Ling <johnlingbusiness@gmail.com>
2025-11-06 15:21:04 +1100

    feat: authentication sets jwt in extension local storage

--- src/plugins/widgets/trello/Trello.tsx
+++ src/plugins/widgets/trello/Trello.tsx
@@ -3,24 +3,6 @@ import { defaultData, Props } from "./types";
 import "./Trello.sass";
 
 const Trello: FC<Props> = ({data = defaultData, setData}) => {
-  const onEvent = (event: MessageEvent<any>) => {
-    if (event.origin === "https://trellocallback-rrswz5h5iq-de.a.run.app") {
-      console.log("Received event");
-      console.log(event.data);
-    }
-  }
-
-  useEffect(() => {
-    console.log("Attaching event listener");
-    window.addEventListener("message", onEvent);
-
-    return () => {
-      console.log("Removing event listener");
-      window.removeEventListener("message", onEvent);
-    }
-  }, []);
-
-
   return (
     <>
     </>
